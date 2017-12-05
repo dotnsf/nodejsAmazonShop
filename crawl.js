fd5d914dd23e9710c4a408225fd84068686f0c56
K.Kimura <dotnsf@jp.ibm.com>
2017-12-05 18:24:02 +0900

    crawler interim updated.

--- crawl.js
+++ crawl.js
@@ -11,8 +11,11 @@
 //. http://www.atmarkit.co.jp/ait/articles/0910/26/news097.html
 
 var cloudantlib = require( 'cloudant' ),
+    crypto = require( 'crypto' ),
     fs = require( 'fs' ),
     request = require( 'request' ),
+    urlencode = require( 'urlencode' ),
+    xml2js = require( 'xml2js' );
 var settings = require( './settings' );
 var cloudant = cloudantlib( { account: settings.cloudant_username, password: settings.cloudant_password } );
 
@@ -136,8 +139,96 @@ function getItemSearchAmazonAPI( node, min, max, page ){
   return new Promise( function( resolve ){
     var totalpages = 0;
     var request_url = 'http://ecs.amazonaws.jp/onca/xml?';
-    var timestamp = 
+    var local_dt = new Date();
+    var dt = local_dt.toISOString();
+    var n = dt.indexOf( '.' );
+    if( n > -1 ){
+      dt = dt.substring( 0, n ) + 'Z';  //. "Y-m-d\TH:i:s\Z"
+    }
+
+    var params = "AWSAccessKeyId=" + settings.aws_key + "&AssociateTag=" + settings.aws_tag + "&BrowseNode=" + node;
+    if( page > 0 ){
+      params += ( "&ItemPage=" + page );
+    }
+    params += ( "&MaximumPrice=" + max + "&MinimumPrice=" + min + "&Operation=ItemSearch&ResponseGroup=ItemAttributes%2CSmall%2CImages&SearchIndex=Beauty&Service=AWSECommerceService&Timestamp=" + urlencode( dt ) + "&Version=2009-01-06" );
+
+    var str = 'GET\necs.amazonaws.jp\n/onca/xml\n' + params;
+
+    var hash = crypto.createHmac( 'sha256', settings.aws_secret );
+    hash.update( str );
+    var hashed_str = hash.digest( 'base64' );
     
+    request_url += ( params + '&Signature=' + urlencode( hashed_str ) );
+
+    var options = {
+      url: request_url,
+      headers: {
+        'User-Agent': 'XXX (Linux)',
+        'Host': 'ecs.amazonaws.jp'
+      },
+      method: 'GET'
+    };
+
+    request( options, ( err, res, body ) => {
+      if( err ){
+        resolve( err );
+      }else{
+        //resolve( body );
+        xml2js.parseString( body, ( err0, xml ) => {
+          if( err0 ){
+            resolve( err0 );
+          }else{
+            if( xml && xml.ItemSearchResponse && xml.ItemSearchResponse.Items ){
+               var Items = xml.ItemSearchResponse.Items[0];
+               var totalpages = Items.TotalPages[0];
+               for( var idx = 0; idx < Items.Item.length; idx ++ ){
+                 var Item = Items.Item[idx];
+                 var image_url = '';
+                 var manufacturer = '';
+                 var brand = '';
+                 var title = '';
+                 var listprice = '';
+                 var ean = '';
+                 var asin = '';
+
+                 try{
+                   image_url = Item.MediumImage[0].URL[0];
+                 }catch(e){
+                 }
+                 try{
+                   manufacturer = Item.ItemAttributes[0].Manufacturer[0];
+                 }catch(e){
+                 }
+                 try{
+                   brand = Item.ItemAttributes[0].Brand[0];
+                 }catch(e){
+                 }
+                 try{
+                   title = Item.ItemAttributes[0].Title[0];
+                 }catch(e){
+                 }
+                 try{
+                   listprice = Item.ItemAttributes[0].ListPrice[0].Amount[0];
+                 }catch(e){
+                 }
+                 try{
+                   ean = Item.ItemAttributes[0].EAN[0];
+                 }catch(e){
+                 }
+                 try{
+                   asin = Item.ASIN[0];
+                 }catch(e){
+                 }
+
+                 if( listprice == '' ){
+                   listprice = 0;
+                 }
+               }
+            }
+          }
+        });
+      }
+    });
   });
 }
 
