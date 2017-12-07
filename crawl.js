3fd871e3439c45f3d0a0c8e7e51d18b45b8cbda9
K.Kimura <dotnsf@jp.ibm.com>
2017-12-07 13:29:08 +0900

    interim fix for crawl.js

--- crawl.js
+++ crawl.js
@@ -1,5 +1,7 @@
 //. crawl.js
 
+var wait = 1300;   //. ウェイト（ミリ秒）
+var outputfilename = 'a.txt';
 
 //. Cloudant REST APIs
 //. https://console.bluemix.net/docs/services/Cloudant/api/database.html#databases
@@ -20,6 +22,7 @@ var settings = require( './settings' );
 var cloudant = cloudantlib( { account: settings.cloudant_username, password: settings.cloudant_password } );
 
 
+fs.writeFile( outputfilename, "", 'utf8', (err) => {});
 function getCodesFromAmazonAPI( node ){
   for( var i = 0; i < 100000; i += 1000 ){
     getCodesAmazonNodeMinMax( $node, i, i + 999 );
@@ -27,36 +30,38 @@ function getCodesFromAmazonAPI( node ){
 }
 
 function getCodesFromAmazonNodeMinMax( node, min, max ){
-  //. Page 1
-  setTimeout( function(){
-    var totalpages = getItemSearchAmazonAPI( node, min, max );
-    if( totalpages < 11 || max - min == 9 ){
-      if( totalpages > 1 ){
-        //. Page 2+
-        var m = ( totalpages > 10 ) ? 10 : totalpages;
-        for( var p = 2; p < m; p ++ ){
-          setTimeout( function(){
-            getItemSearchAmazonAPI( node, min, max, p );
-          }, 1300 );
-        }
-      }
-    }else{
-      //. Page 1+
-      if( max - min == 999 ){
-        for( var i = min; i < max; i += 100 ){
-          getCodesAmazonNodeMinMax( node, i, i + 99 );
-        }
-      }else if( max - min == 99 ){
-        for( var i = min; i < max; i += 10 ){
-          getCodesAmazonNodeMinMax( node, i, i + 9 );
+  return new Promise( function( resolve, reject ){
+    //. Page 1
+    setTimeout( function(){
+      var totalpages = getItemSearchAmazonAPI( node, min, max );
+      if( totalpages < 11 || max - min == 9 ){
+        if( totalpages > 1 ){
+          //. Page 2+
+          var m = ( totalpages > 10 ) ? 10 : totalpages;
+          for( var p = 2; p < m; p ++ ){
+            setTimeout( function(){
+              getItemSearchAmazonAPI( node, min, max, p );
+            }, wait );
+          }
         }
       }else{
-        for( var i = min; i <= max; i ++ ){
-          getCodesAmazonNodeMinMax( node, i, i );
+        //. Page 1+
+        if( max - min == 999 ){
+          for( var i = min; i < max; i += 100 ){
+            getCodesAmazonNodeMinMax( node, i, i + 99 );
+          }
+        }else if( max - min == 99 ){
+          for( var i = min; i < max; i += 10 ){
+            getCodesAmazonNodeMinMax( node, i, i + 9 );
+          }
+        }else{
+          for( var i = min; i <= max; i ++ ){
+            getCodesAmazonNodeMinMax( node, i, i );
+          }
         }
       }
-    }
-  }, 1300 );
+    }, wait );
+  });
 }
 
 
@@ -89,62 +94,74 @@ if( settings.nodes ){
 
 
 function getCodesFromAmazonAPI( node ){
-  for( var i = 0; i < 100000; i += 1000 ){ //. １カテゴリで上限10万円まで調べる
-    getCodesAmazonNodeMinMax( node, i, i + 999 );
-  }
+  return new Promise( function( resolve, reject ){
+    for( var i = 0; i < 100000; i += 1000 ){ //. １カテゴリで上限10万円まで調べる
+      getCodesAmazonNodeMinMax( node, i, i + 999 );
+    }
+  });
 }
 
 function getCodesAmazonNodeMinMax( node, min, max ){
-  getItemSearchAmazonAPIms( node, min, max, 1300 );
+  return new Promise( function( resolve, reject ){
+    resolve( getItemSearchAmazonAPIms( node, min, max, wait ) );
+  });
 }
 
 function getItemSearchAmazonAPIms( node, min, max, ms ){
-  //. Page 1
-  setTimeout( function(){
-    console.log( 'node = ' + node + ' : min = ' + min + ', max = ' + max + ', page = 1 ' );
-    var totalpages = getItemSearchAmazonAPI( node, min, max, 0 );
-    if( totalpages < 11 || max - min == 9 ){
-      if( totalpages > 1 ){
-        //. Page 2+
-        var m = ( totalpages > 10 ) ? 10 : totalpages;
-        for( var p = 2; p <= m; p ++ ){
-          setTimeout( function(){
-            console.log( 'node = ' + node + ' : min = ' + min + ', max = ' + max + ', page = ' + p + ' / ' + totalpages );
-            getItemSearchAmazonAPI( node, min, max, p );
-          }, ms );
-        }
-    }else{
-      //. Page 1+
-      if( max - min == 999 ){
-        for( var i = min; i < max; i += 100 ){
-          getCodesAmazonNodeMinMax( node, i, i + 99 );
-        }
-      }else if( max - min == 99 ){
-        for( var i = min; i < max; i += 10 ){
-          getCodesAmazonNodeMinMax( node, i, i + 9 );
+  return new Promise( function( resolve, reject ){
+    //. Page 1
+    setTimeout( function(){
+      console.log( 'node = ' + node + ' : min = ' + min + ', max = ' + max + ', page = 1 ' );
+      var totalpages = getItemSearchAmazonAPI( node, min, max, 0 );
+      if( totalpages < 11 || max - min == 9 ){
+        if( totalpages > 1 ){
+          //. Page 2+
+          var m = ( totalpages > 10 ) ? 10 : totalpages;
+          for( var p = 2; p <= m; p ++ ){
+            setTimeout( function(){
+              console.log( 'node = ' + node + ' : min = ' + min + ', max = ' + max + ', page = ' + p + ' / ' + totalpages );
+              getItemSearchAmazonAPI( node, min, max, p );
+              if( p == m ){
+                resolve( totalpages );
+              }
+            }, ms );
+          }
+        }else{
+          resolve( totalpages );
         }
       }else{
-        for( var i = min; i < max; i ++ ){
-          getCodesAmazonNodeMinMax( node, i, i );
+        //. Page 1+
+        if( max - min == 999 ){
+          for( var i = min; i < max; i += 100 ){
+            resolve( getCodesAmazonNodeMinMax( node, i, i + 99 ) );
+          }
+        }else if( max - min == 99 ){
+          for( var i = min; i < max; i += 10 ){
+            resolve( getCodesAmazonNodeMinMax( node, i, i + 9 ) );
+          }
+        }else{
+          for( var i = min; i < max; i ++ ){
+            resolve( getCodesAmazonNodeMinMax( node, i, i ) );
+          }
         }
-      }
-    }
-  }, ms );
 
-  return totalpages;
+        resolve( totalpages );
+      }
+    }, ms );
+  });
 }
 
 //. https://github.com/dotnsf/phpAmazonShop/blob/master/crawler/common.php
 function getItemSearchAmazonAPI( node, min, max, page ){
-  return new Promise( function( resolve ){
+  return new Promise( function( resolve, reject ){
     var totalpages = 0;
     var request_url = 'http://ecs.amazonaws.jp/onca/xml?';
     var local_dt = new Date();
     var dt = local_dt.toISOString();
-    var n = dt.indexOf( '.' );
-    if( n > -1 ){
-      dt = dt.substring( 0, n ) + 'Z';  //. "Y-m-d\TH:i:s\Z"
-    }
+//    var n = dt.indexOf( '.' );
+//    if( n > -1 ){
+//      dt = dt.substring( 0, n ) + 'Z';  //. "Y-m-d\TH:i:s\Z"
+//    }
 
     var params = "AWSAccessKeyId=" + settings.aws_key + "&AssociateTag=" + settings.aws_tag + "&BrowseNode=" + node;
     if( page > 0 ){
@@ -179,63 +196,68 @@ function getItemSearchAmazonAPI( node, min, max, page ){
             resolve( err0 );
           }else{
             if( xml && xml.ItemSearchResponse && xml.ItemSearchResponse.Items ){
-               var Items = xml.ItemSearchResponse.Items[0];
-               var totalpages = Items.TotalPages[0];
-               for( var idx = 0; idx < Items.Item.length; idx ++ ){
-                 var Item = Items.Item[idx];
-                 var image_url = '';
-                 var manufacturer = '';
-                 var brand = '';
-                 var title = '';
-                 var listprice = '';
-                 var ean = '';
-                 var asin = '';
+              var Items = xml.ItemSearchResponse.Items[0];
+              var totalpages = Items.TotalPages[0];
+              if( Items.Item && Items.Item.length ){
+                for( var idx = 0; idx < Items.Item.length; idx ++ ){
+                  var Item = Items.Item[idx];
+                  var image_url = '';
+                  var manufacturer = '';
+                  var brand = '';
+                  var title = '';
+                  var listprice = '';
+                  var ean = '';
+                  var asin = '';
 
-                 try{
-                   image_url = Item.MediumImage[0].URL[0];
-                 }catch(e){
-                 }
-                 try{
-                   manufacturer = Item.ItemAttributes[0].Manufacturer[0];
-                 }catch(e){
-                 }
-                 try{
-                   brand = Item.ItemAttributes[0].Brand[0];
-                 }catch(e){
-                 }
-                 try{
-                   title = Item.ItemAttributes[0].Title[0];
-                 }catch(e){
-                 }
-                 try{
-                   listprice = Item.ItemAttributes[0].ListPrice[0].Amount[0];
-                 }catch(e){
-                 }
-                 try{
-                   ean = Item.ItemAttributes[0].EAN[0];
-                 }catch(e){
-                 }
-                 try{
-                   asin = Item.ASIN[0];
-                 }catch(e){
-                 }
+                  try{
+                    image_url = Item.MediumImage[0].URL[0];
+                  }catch(e){
+                  }
+                  try{
+                    manufacturer = Item.ItemAttributes[0].Manufacturer[0];
+                  }catch(e){
+                  }
+                  try{
+                    brand = Item.ItemAttributes[0].Brand[0];
+                  }catch(e){
+                  }
+                  try{
+                    title = Item.ItemAttributes[0].Title[0];
+                  }catch(e){
+                  }
+                  try{
+                    listprice = Item.ItemAttributes[0].ListPrice[0].Amount[0];
+                  }catch(e){
+                  }
+                  try{
+                    ean = Item.ItemAttributes[0].EAN[0];
+                  }catch(e){
+                  }
+                  try{
+                    asin = Item.ASIN[0];
+                  }catch(e){
+                  }
+ 
+                  if( listprice == '' ){
+                    listprice = 0;
+                  }
 
-                 if( listprice == '' ){
-                   listprice = 0;
-                 }
+                  if( asin ){
+                    var item = { asin: asin };
+                    if( ean ){ item['code'] = ean; }
+                    if( title ){ item['name'] = title; }
+                    if( listprice ){ item['price'] = listprice; }
+                    if( brand ){ item['brand'] = brand; }
+                    if( manufacturer ){ item['maker'] = manufacturer; }
+                    if( image_url ){ item['image_url'] = image_url; }
+ 
+                    var line = JSON.stringify( item, 2, null );
+                    console.log( line );
 
-                 if( asin ){
-                   var item = { asin: asin };
-                   if( ean ){ item['code'] = ean; }
-                   if( title ){ item['name'] = title; }
-                   if( listprice ){ item['price'] = listprice; }
-                   if( brand ){ item['brand'] = brand; }
-                   if( manufacturer ){ item['maker'] = manufacturer; }
-                   if( image_url ){ item['image_url'] = image_url; }
-
-                   console.log( JSON.stringify( item, 2, null ) );
-                 }
-               }
+                    fs.appendFile( outputfilename, line + "\n", 'utf8', (err) => {});
+                  }
+                }
+              }
             }
           }
         });
